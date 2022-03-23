0f8624e62d442e4ec78ab6b5653edf3979e107e3
samiulalimsaad <saadnbiuraj@gmail.com>
2022-03-23 15:59:51 +0600

    layout added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css";
-import Navbar from "./Navbar";
+import Navbar from "./navbar";
+import Product from "./product";
+import Summary from "./summary";
 
 function App() {
     return (
         <div>
             <Navbar />
+            <main className="main">
+                <Product />
+                <Summary />
+            </main>
         </div>
     );
 }

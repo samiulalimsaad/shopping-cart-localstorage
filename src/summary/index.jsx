d2a68df91f6473f3b862bc2d441b00e1849a9600
samiulalimsaad <saadnbiuraj@gmail.com>
2022-04-02 19:01:39 +0600

    added summary layout

--- src/summary/index.jsx
+++ src/summary/index.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { FaArrowRight, FaTrashRestoreAlt } from "react-icons/fa";
+import "./summary.css";
 
 const Summary = () => {
-    return <div>Summary</div>;
+    return (
+        <div className="summary-container">
+            <h2>Order Summary</h2>
+            <div className="order-details">
+                <p>Selected Items: 10</p>
+                <p>Total Price: $10</p>
+                <p>Total Shipping Charge: $10</p>
+                <p>Tax: $10</p>
+                <h3>Grand Total: $10</h3>
+
+                <div>
+                    <button>
+                        Clear Cart{" "}
+                        <span>
+                            <FaTrashRestoreAlt />
+                        </span>
+                    </button>
+                    <button>
+                        Review Order{" "}
+                        <span>
+                            <FaArrowRight />
+                        </span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Summary;
